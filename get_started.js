aa46fa9932dd490250450520a2c971adbbbfef47
Guillaume Pichot <guillaume.pichot@metapack.com>
2020-03-03 10:29:48 +0000

    added function cases to workshop

--- get_started.js
+++ get_started.js
@@ -1,10 +1,34 @@
-const assert = require('assert');
+import assert from 'assert';
 
+// Function 'log' that prints a message to the console
 log = arg => {console.log(arg)};
 
 log("Javascript in Action!");
 
+// 1 - Basic Arithmetic functions
+
 // Identity function: function that always returns the same value that was used as its argument
 
-// assert(identity(1) ==1);
+
+
+assert(identity(1) ==1);
+log(identity(2));
+
+// Function 'add' that adds two numbers is the total of those values combined
+
+// Function 'sub' that substracts two numbers:
+
+// Function 'mul' that multiplies two numbers:
+
+// 2 - Working with Functions
+
+// Function 'identityf' of 'a' that returns a function identity of 'a'
+
+// Function 'addf' of 'a' that returns a function of 'b' that adds 'a' and 'b'
+
+// Function 'liftf' of a binary function that returns a function of 'a' 
+// that returns a function of 'b' that executes the binary function on 'a' and 'b'
+
+
+
 
