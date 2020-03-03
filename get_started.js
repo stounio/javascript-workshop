a7a562e368399dc11bd31f8e58ebaa5523f73e70
Guillaume Pichot <guillaume.pichot@metapack.com>
2020-03-03 09:27:35 +0000

    Setup Javascript environment with test harness

---
+++ get_started.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+log = arg => {console.log(arg)};
+
+log("Javascript in Action!");
+
+// Identity function: function that always returns the same value that was used as its argument
+
+// assert(identity(1) ==1);
+
